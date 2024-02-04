048745368b3e6e88b5d9d39a336cb65a7004b3b3
Andrey999K <andrey1234xxxx@mail.ru>
2024-02-05 02:43:38 +0300

    feat: add hook useToggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import * as React from 'react'
-import {useFetch} from "../hooks/useFetch";
-import {DataItem} from "../types";
-import {useEffect} from "react";
+import * as React from "react"
+import { useToggle } from "./hooks/useToggle";
 
 export function App() {
-  const {
-    data,
-    isLoading,
-    error,
-    refetch
-  } = useFetch('https://jsonplaceholder.typicode.com/posts');
-
-  useEffect(() => {
-    console.log({ data });
-  }, [data]);
+  const [value, toggle] = useToggle(["blue", "orange", "cyan", "teal"]);
 
   return (
-    <div>
-      <div>
-        <button onClick={() => refetch({
-          params: {
-            _limit: 3
-          }
-        })}>
-          Перезапросить
-        </button>
-      </div>
-      {isLoading && 'Загрузка...'}
-      {error && 'Произошла ошибка'}
-      {console.log(data)}
-      {data && !isLoading && data.map((item: DataItem) => <div key={item.id}>{item.title}</div>) }
-    </div>
+    <button onClick={() => toggle()}>
+      {value}
+    </button>
   );
 }
