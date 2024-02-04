ec885ef93bd8951b6543289a478ebce61c48eb90
Andrey999K <andrey1234xxxx@mail.ru>
2024-02-04 21:06:53 +0300

    feat: add hook useHover

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import * as React from 'react'
-import {useFetch} from "../hooks/useFetch";
-import {DataItem} from "../types";
-import {useEffect} from "react";
+import * as React from "react"
+import { useHover } from "./hooks/useHover";
 
 export function App() {
-  const {
-    data,
-    isLoading,
-    error,
-    refetch
-  } = useFetch('https://jsonplaceholder.typicode.com/posts');
-
-  useEffect(() => {
-    console.log({ data });
-  }, [data]);
+  const { hovered, ref } = useHover();
 
   return (
-    <div>
-      <div>
-        <button onClick={() => refetch({
-          params: {
-            _limit: 3
-          }
-        })}>
-          Перезапросить
-        </button>
-      </div>
-      {isLoading && 'Загрузка...'}
-      {error && 'Произошла ошибка'}
-      {console.log(data)}
-      {data && !isLoading && data.map((item: DataItem) => <div key={item.id}>{item.title}</div>) }
+    <div ref={ref}>
+      {hovered ? "На меня навели мышку" : "Наведи мышкой на меня"}
     </div>
   );
 }
