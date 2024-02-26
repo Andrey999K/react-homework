a2af80a7f84df5951248dff6b374dce75a55fa23
Andrey999K <andrey1234xxxx@mail.ru>
2024-02-26 23:00:40 +0300

    feat: add sort by created in EpisodesList and LocationsList

--- src/pages/locations/LocationsList/index.tsx
+++ src/pages/locations/LocationsList/index.tsx
@@ -1,17 +1,29 @@
 import React from "react";
 import locations from "../../../mock/locations.json";
 import { Link } from "react-router-dom";
+import useSort from "../../../hooks/useSort";
+import sortByDateCreated from "../../../utils/sortByDateCreated";
+import Button from "../../../components/common/Button";
+import { convertDataTime } from "../../../utils/convertDataTime";
 
 const LocationsList = () => {
-  console.log(locations);
+  const { sortByCreated, handlerToggle } = useSort();
   return (
-    <ul className="list">
-      {locations.map(item => (
-        <li key={item.id}>
-          <Link to={`/locations/${item.id}`}>{item.name}</Link>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <div className="button-wrapper">
+        <Button onClick={handlerToggle}>{sortByCreated}</Button>
+      </div>
+      <ul className="list">
+        {sortByDateCreated(locations, sortByCreated).map(item => (
+          <li key={item.id}>
+            <Link to={`/locations/${item.id}`} className="list__link">
+              <span className="list__name">{item.name}</span>
+              <span className="list__datetime">{convertDataTime(item.created)}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
