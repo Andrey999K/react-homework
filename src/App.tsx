f61baad182582f3ad118a96817c0d5ab85730486
Andrey999K <andrey1234xxxx@mail.ru>
2024-04-09 03:23:31 +0300

    feat: add component AppRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,8 @@
 import * as React from "react";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import CharactersList from "./pages/characters/CharactersList";
-import EpisodesList from "./pages/episodes/EpisodesList";
-import LocationsList from "./pages/locations/LocationsList";
-import MainLayout from "./layouts/MainLayout";
-import CharacterPage from "./pages/characters/CharacterPage";
-import EpisodePage from "./pages/episodes/EpisodePage";
-import LocationPage from "./pages/locations/LocationPage";
+import AppRouter from "./components/ui/AppRouter";
 
 export function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="characters">
-            <Route index element={<CharactersList />} />
-            <Route path=":characterId" element={<CharacterPage />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-          <Route path="episodes">
-            <Route index element={<EpisodesList />} />
-            <Route path=":episodeId" element={<EpisodePage />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-          <Route path="locations">
-            <Route index element={<LocationsList />} />
-            <Route path=":locationId" element={<LocationPage />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </>
+    <AppRouter />
   );
 }
---
+++ src/components/ui/AppRouter/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import MainLayout from "../../../layouts/MainLayout";
+import Home from "../../../pages/Home";
+import CharactersList from "../../../pages/characters/CharactersList";
+import CharacterPage from "../../../pages/characters/CharacterPage";
+import NotFound from "../../../pages/NotFound";
+import EpisodesList from "../../../pages/episodes/EpisodesList";
+import EpisodePage from "../../../pages/episodes/EpisodePage";
+import LocationsList from "../../../pages/locations/LocationsList";
+import LocationPage from "../../../pages/locations/LocationPage";
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="characters">
+          <Route index element={<CharactersList />} />
+          <Route path=":characterId" element={<CharacterPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+        <Route path="episodes">
+          <Route index element={<EpisodesList />} />
+          <Route path=":episodeId" element={<EpisodePage />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+        <Route path="locations">
+          <Route index element={<LocationsList />} />
+          <Route path=":locationId" element={<LocationPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default AppRouter;
