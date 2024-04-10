8334135237a38eb4345dfd0af90c0742a8e71e8b
Andrey999K <andrey1234xxxx@mail.ru>
2024-04-10 20:50:26 +0300

    feat: add get a single character in CharacterPage

--- src/pages/characters/CharacterPage/index.tsx
+++ src/pages/characters/CharacterPage/index.tsx
@@ -1,14 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import characters from "../../../mock/characters.json";
 import { useParams } from "react-router-dom";
-import { ObjectDefault } from "../../../types";
+import { Character, ObjectDefault } from "../../../types";
 import "./CharacterPage.scss";
+import axios from "axios";
 
 export const CharacterPage = () => {
+  const [character, setCharacter] = useState<ObjectDefault | null>();
   const { characterId } = useParams();
-  const character: ObjectDefault = characters.find(
-    item => item.id.toString() === characterId
-  );
+  const [loading, setLoading] = useState(true);
+  // const character: ObjectDefault = characters.find(
+  //   item => item.id.toString() === characterId
+  // );
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: `https://rickandmortyapi.com/api/character/${characterId}`
+    }).then(res => {
+      setCharacter(res.data);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) return <div className="text-center w-full">Loading...</div>;
+
   return (
     <div className="character">
       <img
@@ -18,12 +33,13 @@ export const CharacterPage = () => {
       />
       <ul className="list">
         {Object.keys(character).map(item => {
-          if (item !== "image")
+          if (item !== "image" && typeof character[item] === "string") {
             return (
               <li key={item}>
                 <b>{item}:</b> <span>{character[item]}</span>
               </li>
             );
+          }
         })}
       </ul>
     </div>
