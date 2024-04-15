a2dfd8efcc30c030a02f2611bac334f0a812c74d
Andrey999K <andrey1234xxxx@mail.ru>
2024-04-15 17:49:21 +0300

    refactor: add changes after rebase on development

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,10 +1,14 @@
 import { useState } from "react";
 
-export function useToggle(states: Array<string> = ["true", "false"], startValue?: number | string): [string, () => void] {
+export function useToggle(
+  states: Array<string> = ["true", "false"],
+  startValue?: number | string
+): [string, () => void] {
   const [index, setIndex] = useState<number>(
-    typeof startValue === "string" ? states.findIndex(state => state === startValue) : 0
+    typeof startValue === "string"
+      ? states.findIndex(state => state === startValue)
+      : 0
   );
-
   const toggle = (): void => {
     setIndex(prevState => (prevState >= states.length - 1 ? 0 : prevState + 1));
   };
