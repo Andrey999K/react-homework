3f43aab6cc139a0630f8e7f9df9d12f1dbf315df
Andrey999K <andrey1234xxxx@mail.ru>
2024-06-05 01:47:18 +0300

    feat: add dynamic cache

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const staticCacheName = "static-site";
+const dynamicCacheName = "dynamic-site";
 
 const ASSETS = ["/", "/index.html"];
 
@@ -10,15 +11,28 @@ self.addEventListener("install", async event => {
 });
 
 // activate event
-self.addEventListener("activate", event => {
+self.addEventListener("activate", async event => {
+  // const cachesKeysArr = await caches.keys();
   console.log("####: Service Worker had been activated");
+  // await Promise.all(
+  //   cachesKeysArr
+  //     .filter(key => key !== staticCacheName)
+  //     .map(key => caches.delete(key))
+  // );
 });
 
 // fetch event
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches
-      .match(event.request)
-      .then(cacheRes => cacheRes || fetch(event.request))
+    caches.match(event.request).then(
+      cacheRes =>
+        cacheRes ||
+        fetch(event.request).then(response => {
+          return caches.open(dynamicCacheName).then(cache => {
+            cache.put(event.request.url, response.clone());
+            return response;
+          });
+        })
+    )
   );
 });
