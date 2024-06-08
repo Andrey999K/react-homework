3cf46d544c945c83aebb15d11d21cbadfcacc1f4
Andrey999K <andrey1234xxxx@mail.ru>
2024-06-08 03:43:28 +0300

    fix: fix other error

--- hooks/useFetch.ts
+++ hooks/useFetch.ts
@@ -1,16 +1,16 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 interface FetchState {
-  data: any,
-  isLoading: boolean,
-  error: Error | null,
-  refetch: RefetchFunction
+  data: any;
+  isLoading: boolean;
+  error: Error | null;
+  refetch: RefetchFunction;
 }
 
 type RefetchParams = {
   params: {
-    [key: string]: string | number
-  }
+    [key: string]: string | number;
+  };
 };
 
 type RefetchFunction = (params: RefetchParams) => void;
@@ -22,7 +22,7 @@ export function useFetch(url: string): FetchState {
 
   const refetch = ({ params }: RefetchParams): void => {
     let newParams = {};
-    for (let field in params) {
+    for (const field in params) {
       newParams = { ...newParams, [field]: params[field].toString() };
     }
     url += "?" + new URLSearchParams(newParams).toString();
